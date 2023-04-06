9afa3dbf82dc781ca1382959488ac3989086a7cf
HarenKei <seungmin1266@gmail.com>
2023-04-06 17:39:22 +0900

    Update Header.tsx : Implement Header UI

--- src/Common/Header.tsx
+++ src/Common/Header.tsx
@@ -1,19 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "./Logo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-    return(
-          <HeaderContainer>
-            <p>헤더</p>
-          </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <LogoContainer>
+        <Logo width={168} height={52.5} />
+      </LogoContainer>
+
+      <HambugerMenuContainer>
+        <FontAwesomeIcon icon={faBars} size={"2x"} />
+      </HambugerMenuContainer>
+    </HeaderContainer>
+  );
 };
 
 const HeaderContainer = styled.div`
-    width: 100%;
-    height: 6vh;
-    background-color: grey;
+  width: 100%;
+  height: 6vh;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
+const LogoContainer = styled.div`
+  margin-left: 80px;
+`;
+
+const HambugerMenuContainer = styled.div`
+  margin-right: 80px;
+`;
 
 export default Header;
