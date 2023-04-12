1157025d8aad8b70870c53e26e04ae188feaaf3b
kakao3450 <dlwjddnr3450@naver.com>
2023-04-12 14:43:29 +0900

    Add
    NoticPost에 Router추가.  전공 선택 시 각각의 페이지 호출

--- pages/NoticePost/NoticeMajor.tsx
+++ pages/NoticePost/NoticeMajor.tsx
@@ -1,25 +1,50 @@
 import React from "react";
 import styled from "styled-components";
-
+import { Link, Routes, Route } from "react-router-dom";
+import AppContainer from "./AppContainer";
+import ApplicationContainer from "./ApplicationContainer";
+import NoticeBodyContainer from "./NoticeBodyContainer";
 const NoticeMajor = () => {
   return (
     <div>
-      <ContentMajor>
+      {/* <ContentMajor>
         <MajorContent><h3>웹 전공</h3></MajorContent>
         <MajorContent><h3>앱 전공</h3></MajorContent>
         <MajorContent><h3>응용 전공</h3></MajorContent>
+      </ContentMajor> */}
+      <ContentMajor>
+        <MajorContent>
+          <Link to="/">
+            <h3>웹 전공</h3>
+          </Link>
+        </MajorContent>
+        <MajorContent>
+          <Link to="/AppContainer">
+            <h3>앱 전공</h3>
+          </Link>
+        </MajorContent>
+        <MajorContent>
+          <Link to="/ApplicationContainer">
+            <h3>응용 전공</h3>
+          </Link>
+        </MajorContent>
       </ContentMajor>
+      <Routes>
+        <Route path="/" element={<NoticeBodyContainer/>} />
+        <Route path="/AppContainer" element={<AppContainer />} />
+        <Route path="/ApplicationContainer" element={<ApplicationContainer />}/>
+      </Routes>
     </div>
   );
 };
 const MajorContent = styled.div`
-  background-color: #D9D9D9;
+  background-color: #d9d9d9;
   padding: 1rem;
   width: 13rem;
   max-width: 95%;
   border-radius: 12px;
   text-align: center;
-  box-shadow:  0px 1px 1px black;
+  box-shadow: 0px 1px 1px black;
 `;
 const ContentMajor = styled.div`
   justify-content: space-around;
@@ -29,7 +54,7 @@ const ContentMajor = styled.div`
   width: 60vw;
   max-width: 95%;
   & > div:hover {
-    background-color: #ADABAB;
+    background-color: #adabab;
   }
 `;
 export default NoticeMajor;
