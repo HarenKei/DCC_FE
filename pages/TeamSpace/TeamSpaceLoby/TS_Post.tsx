7526fd18b2032ae8f9e63dff8f2aab89091a8307
Anhagu <leejhsam330@gmail.com>
2023-04-12 15:57:24 +0900

    팀 스페이스 메인 추가

---
+++ pages/TeamSpace/TeamSpaceLoby/TS_Post.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import TS_AddButton from "./TS_AddButton";
+
+const TS_Post = () => {
+  return (
+    <div>
+      <ContentContainer>
+        <TeamSpaceContent><h3>프로젝트</h3></TeamSpaceContent>
+        <TeamSpaceContent><h3>스터디</h3></TeamSpaceContent>
+        <TeamSpaceContent><h3>동아리</h3></TeamSpaceContent>
+        <TS_AddButton/>
+      </ContentContainer>
+    </div>
+  );
+};
+const TeamSpaceContent = styled.div`
+  background-color: #D9D9D9;
+  padding: 1rem;
+  width: 13rem;
+  max-width: 95%;
+  border-radius: 12px;
+  text-align: center;
+  box-shadow:  0px 1px 1px black;
+`;
+const ContentContainer = styled.div`
+  justify-content: space-around;
+  display: flex;
+  padding: 1rem;
+  margin: 2rem auto;
+  width: 60vw;
+  max-width: 95%;
+  & > div:hover {
+    background-color: #ADABAB;
+  }
+`;
+export default TS_Post;
