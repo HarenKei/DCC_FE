7fe724bde8917ded9a6cec2f854c2856e4039e0d
HarenKei <seungmin1266@gmail.com>
2023-05-08 00:20:53 +0900

    commit for pull

---
+++ pages/TimeTable/Table.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import { useTable } from "react-table";
+
+const Table = () => {
+    return(
+        <TableContainer>
+        </TableContainer>
+    );
+};
+
+const TableContainer = styled.div`
+    
+`;
+
+export default Table;
+
