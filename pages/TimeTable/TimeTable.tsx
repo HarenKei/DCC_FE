983bacde29f953339ae1575956c7248e5f67a0d0
HarenKei <seungmin1266@gmail.com>
2023-05-26 01:13:40 +0900

    Update TimeTable.tsx : Connect FireStore DB & Implement Create Function

--- pages/TimeTable/TimeTable.tsx
+++ pages/TimeTable/TimeTable.tsx
@@ -1,7 +1,23 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, use } from "react";
 import styled from "styled-components";
 import DayView from "./DayView";
 import TimeTableModal from "./TimeTableModal";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+import { db, auth, storage } from "../Google2/fbconfig";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 
 const dummyDataArray = [
   {
@@ -38,6 +54,52 @@ const TimeTable = () => {
   const no = useRef(dummyDataArray.length + 1); // 고유 id가 될 no는 데이터.length +1
   const [classData, setClassData] = useState(dummyDataArray); // 데이터 상태
   const [modalOpen, setModalOpen] = useState(false);
+  const [userId, setUserId] = useState("asdf");
+  const [userName, setUserName] = useState("asdf");
+  const timeTableCollectionRef = collection(db, `Users/${userId}/TimeTable`);
+
+  const getTimeTableList = async () => {
+    try {
+      const data = await getDocs(timeTableCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setClassData(filteredData);
+      console.log(`getTimeTableList ${classData}`);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const onSubmitMovie = async (tmp : any) => {
+    try {
+        await addDoc(timeTableCollectionRef,{
+            ...tmp,
+        });
+        getTimeTableList();
+        console.log(`onSubmit ${classData}`);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        // User logged in already or has just logged in.
+        console.log(user.uid + "" + user.displayName);
+        setUserId(user.uid);
+        setUserName(user.displayName);
+        const UsersDocRef = doc(db, "Users", userId);
+        // await addUsers(UsersDocRef);
+      } else {
+        // User not logged in or has just logged out.
+      }
+    });
+    console.log(`userID : ${userId} userName : ${userName}`);
+    getTimeTableList();
+  }, [userId]);
 
   const openModal = () => {
     setModalOpen(true);
@@ -47,6 +109,7 @@ const TimeTable = () => {
     form.id = no.current++;
     let tmp = classData.slice();
     tmp.push(form);
+    onSubmitMovie(form);
     setClassData(tmp);
   };
 
